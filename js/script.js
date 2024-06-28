eb7e36cdc64eaf1241ff3539f9163ace7c63ee0f
Secelia McNair <secelia@yahoo.com>
2024-06-28 11:08:33 -0500

    updated changes to Leon's original code

--- js/script.js
+++ js/script.js
@@ -1,25 +1,17 @@
-document.querySelector("#purple").addEventListener("click", partyPurple);
-document.querySelector("#green").addEventListener("click", partyGreen);
-document.querySelector("#blue").addEventListener("click", partyBlue);
-document.querySelector("#orange").addEventListener("click", partyOrange);
+document.getElementById("purple").onclick = partyPurple;
+ddocument.getElementById("green").onclick = partyGreen;
+document.getElementById("blue").onclick = partyBlue;
 
 function partyPurple() {
-  document.body.style.backgroundColor = "rgba(241,63,247,1)";
-  document.body.style.color = "white";
-
+  document.querySelector('body').style.backgroundColor = "rgba(241,63,247,1)";
+  document.querySelector('body').style.color = "white";
 }
 function partyGreen() {
-  document.body.style.backgroundColor = "rgba(0,253,81,1)";
-  document.body.style.color = "white";
-
+  document.querySelector("body").style.backgroundColor = "rgba(0,253,81,1)";
+  document.querySelector("body").style.color = "white";
 }
 
 function partyBlue() {
-  document.body.style.backgroundColor = "rgba(0,254,255)";
-  document.body.style.color = "white";
-}
-
-function partyOrange() {
-  document.body.style.backgroundColor = "rgb(255, 69, 0)";
-  document.body.style.color = "white"
+  document.querySelector("body").style.backgroundColor = "rgba(0,254,255,1)";
+  document.querySelector("body").style.color = "white";
 }
